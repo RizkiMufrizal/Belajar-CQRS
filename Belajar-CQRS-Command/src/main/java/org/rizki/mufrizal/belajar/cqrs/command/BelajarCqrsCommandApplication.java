26537ee27cf79f3d512e74986b9ef6f2737b30f1
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-10-05 22:54:25 +0700

    intial project

---
+++ Belajar-CQRS-Command/src/main/java/org/rizki/mufrizal/belajar/cqrs/command/BelajarCqrsCommandApplication.java
@@ -0,0 +1,13 @@
+package org.rizki.mufrizal.belajar.cqrs.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarCqrsCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarCqrsCommandApplication.class, args);
+	}
+
+}
---
+++ Belajar-CQRS-Query/src/main/java/org/rizki/mufrizal/belajar/cqrs/query/BelajarCqrsQueryApplication.java
@@ -0,0 +1,13 @@
+package org.rizki.mufrizal.belajar.cqrs.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarCqrsQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarCqrsQueryApplication.class, args);
+	}
+
+}
